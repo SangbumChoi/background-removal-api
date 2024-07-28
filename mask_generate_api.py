deb8fb3b8c9c461faae24acfefeb4d6b7fbc6a0c
최상범 <choisangbum@choesangbeoms-MacBook-Pro.local>
2024-07-28 16:07:14 +0900

    minor change

--- mask_generate_api.py
+++ mask_generate_api.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify, request
 from InSpyReNet.inspyrenet import process_image, check_alpha_layer, get_polygon_from_mask
-from RepViT.repvit import repvit
-import cv2
-import numpy as np
+# from RepViT.repvit import repvit
+# import cv2
+# import numpy as np
 import json
 from PIL import Image
 import io
@@ -36,29 +36,29 @@ def hello_world():
     return 'This API is mask generator using machine learning model.'
 
 # JSON 데이터를 반환하는 엔드포인트
-@app.route('/api/rep_vit/point_based_mask', methods=['POST'])
-def generate_rep_vit_mask():
-    # 요청에서 파일이 있는지 확인
-    if 'image' not in request.files:
-        return jsonify({'error': 'No image file in request'}), 400
+# @app.route('/api/rep_vit/point_based_mask', methods=['POST'])
+# def generate_rep_vit_mask():
+#     # 요청에서 파일이 있는지 확인
+#     if 'image' not in request.files:
+#         return jsonify({'error': 'No image file in request'}), 400
     
-    image_file = request.files['image']
+#     image_file = request.files['image']
 
-    # 이미지 파일을 메모리에서 읽음
-    image_bytes = np.fromstring(image_file.read(), np.uint8)
-    image = cv2.cvtColor(cv2.imdecode(image_bytes, cv2.IMREAD_COLOR), cv2.COLOR_BGR2RGB)
+#     # 이미지 파일을 메모리에서 읽음
+#     image_bytes = np.fromstring(image_file.read(), np.uint8)
+#     image = cv2.cvtColor(cv2.imdecode(image_bytes, cv2.IMREAD_COLOR), cv2.COLOR_BGR2RGB)
 
-    try:
-        ort_outputs = repvit(image=image)
-        json_outputs = json.dumps({'ort_outputs': ort_outputs.tolist()})
+#     try:
+#         ort_outputs = repvit(image=image)
+#         json_outputs = json.dumps({'ort_outputs': ort_outputs.tolist()})
 
-        response = {
-            'message': 'Image received',
-            'ort_outputs': json_outputs
-        }
-        return jsonify(response), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+#         response = {
+#             'message': 'Image received',
+#             'ort_outputs': json_outputs
+#         }
+#         return jsonify(response), 200
+#     except Exception as e:
+#         return jsonify({'error': str(e)}), 500
     
 # JSON 데이터를 반환하는 엔드포인트
 @app.route('/api/inspyrenet/point_based_mask', methods=['POST'])
@@ -99,8 +99,11 @@ def generate_inspyrenet_mask():
         return jsonify({'error': str(e)}), 500
     
 
-# /usr/bin/python3 /Users/choisangbum/Downloads/background-removal-api/mask_generate_api.py
-# curl -X POST http://127.0.0.1:5000/api/rep_vit/point_based_mask -F "image=@/Users/choisangbum/Downloads/background-removal-api/examples/example1.jpg"
-# curl -X POST http://127.0.0.1:5000/api/inspyrenet/point_based_mask -F "image=@/Users/choisangbum/Downloads/background-removal-api/examples/example1.jpg" -F "x=10" -F "y=20"
+'''
+/usr/bin/python3 /Users/choisangbum/Downloads/background-removal-api/mask_generate_api.py
+curl -X POST http://127.0.0.1:5000/api/rep_vit/point_based_mask -F "image=@/Users/choisangbum/Downloads/background-removal-api/examples/example1.jpg"
+curl -X POST http://127.0.0.1:5000/api/inspyrenet/point_based_mask -F "image=@/Users/choisangbum/Downloads/background-removal-api/examples/example1.jpg" -F "x=1280" -F "y=900"
+curl -X POST http://127.0.0.1:5000/api/inspyrenet/point_based_mask -F "image=@/Users/choisangbum/Downloads/background-removal-api/examples/example2.jpg" -F "x=1200" -F "y=750"
+'''
 if __name__ == '__main__':
     app.run(debug=True)
